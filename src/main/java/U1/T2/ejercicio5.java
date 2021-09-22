8b6b499ada967281b3f1046f78cf3a8b2ac8c0bb
Juan <alum.jluishernandezt@iesalixar.org>
2021-09-22 17:47:55 +0200

    Ejericios

--- src/main/java/U1/T2/ejercicio5.java
+++ src/main/java/U1/T2/ejercicio5.java
@@ -5,6 +5,13 @@ import java.util.Scanner;
 // calcule (y muestre) a cuántos grados Fahrenheit equivalen (F = 9*C/5 + 32).
 public class ejercicio5 {
     public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
 
+        System.out.println("Dime los grados: ");
+        double grados = teclado.nextDouble();
+
+        double gradosFahreheim = (9 * grados / 5 ) + 32;
+
+        System.out.println("Los grados Fahrenheit son: " + gradosFahreheim);
     }
 }
