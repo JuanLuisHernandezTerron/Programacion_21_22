c27bd8ebd44bc7901d7dea565f0d2c69a8e22304
JuanLuisHernandezTerron <alum.jluishernandezt@iesalixar.org>
2021-11-11 13:46:26 +0100

    ejercicios

--- src/main/java/U3/T3/Ejercicio5.java
+++ src/main/java/U3/T3/Ejercicio5.java
@@ -11,11 +11,34 @@ public class Ejercicio5 {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
 
-        int espacioArray[] = {1,6,5,9,2,7};
-        int espacioArray2[] = {7,5,6,7,2,3};
-        int arrayOrdenado [] = new int [6];
+        int []espacioArray = {1,6,5,9,2,7};
+        int []espacioArray2 = {7,5,6,7,2,3};
+        int []arrayOrdenado = new int [espacioArray.length + espacioArray2.length];
+        System.arraycopy(espacioArray,0,arrayOrdenado,0,espacioArray.length);
+        System.arraycopy(espacioArray2,0,arrayOrdenado,espacioArray2.length,espacioArray2.length);
+        ordenarArrays(espacioArray);
+        ordenarArrays(espacioArray2);
+        ordenarArrays(arrayOrdenado);
+    }
 
+    private static void ordenarArrays(int[] arrays) {
+        boolean es_Ordenado = false;
 
+        while (es_Ordenado == false){
+            int contadorCambios = 0;
+            for (int i = 0; i < arrays.length-1; i++) {
+                if (arrays[i] > arrays[i+1]){
+                    int numAUX = arrays[i];
+                    arrays[i] = arrays[i+1];
+                    arrays[i+1] = numAUX;
+                    contadorCambios ++;
+                }
+            }
 
+            if (contadorCambios == 0){
+                es_Ordenado = true;
+            }
+        }
+        System.out.println(Arrays.toString(arrays));
     }
 }
