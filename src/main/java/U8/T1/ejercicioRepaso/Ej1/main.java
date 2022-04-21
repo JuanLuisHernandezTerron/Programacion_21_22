2b6501c6a3b0d2e0b2da9710c202ec413e138e2f
JuanLuisHernandezTerron <alum.jluishernandezt@iesalixar.org>
2022-04-21 18:13:58 +0200

    ejercicios

---
+++ src/main/java/U8/T1/ejercicioRepaso/Ej1/main.java
@@ -0,0 +1,25 @@
+package U8.T1.ejercicioRepaso.Ej1;
+
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.IOException;
+
+public class main {
+    public static void main(String[] args) throws SAXException {
+        System.out.println();
+        try{
+            SAXParserFactory spf = SAXParserFactory.newInstance();
+            SAXParser saxParser = spf.newSAXParser();
+            DefaultHandler personSax = new personSax();
+            saxParser.parse("src/main/java/U8/T1/ejercicioRepaso/U8.T1.ejercicioRepaso.Ej1/personas.xml",personSax);
+        }catch (ParserConfigurationException | IOException | SAXException ex){
+            System.out.println(ex.getStackTrace());
+        }
+    }
+
+
+}
